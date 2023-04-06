b31b2d0f0dae088700a74e7dcf664810a1ab100e
teinett <teine@teine.ru>
2023-04-06 01:03:45 -0600

    Update astro.config.mjs
    Added
    - site domain
    - integration for .md and .mdx files
    - settings for trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://untitled.urbansheep.com',
+    trailingSlash: 'never',
+    integrations: [mdx()]
+});
