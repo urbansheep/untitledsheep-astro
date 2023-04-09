2efaf8e288868049f9914922ceaae9d431b12d16
teinett <teine@teine.ru>
2023-04-09 01:15:29 -0600

    Create config.ts

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+// Consts for site and theme configuration
+export const SITE = {
+    name: 'untitled',
+    title: '*Безымянная овца*',
+    description: 'Untitled Urbansheep',
+    url: 'https://untitled.urbansheep.com',
+}
+
+// Theme configuration
+export const THEME = {
+    // Theme: Posts per page (pagination)
+    postsPerPage: 5,
+}
+
