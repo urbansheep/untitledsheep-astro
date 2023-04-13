f376be00aa496b4dc71974bca29a854f4dfd3870
teinett <teine@teine.ru>
2023-04-13 00:59:24 -0600

    Small fixes after linting

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,10 @@ import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
 
 export default defineConfig({
-  site: 'https://untitled.urbansheep.com',
-  integrations: [mdx()],
-  output: 'server',
-  adapter: node({
-    mode: "standalone"
-  })
+    site: 'https://untitled.urbansheep.com',
+    integrations: [mdx()],
+    output: 'server',
+    adapter: node({
+        mode: "standalone"
+    })
 });
