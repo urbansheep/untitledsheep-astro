3e984d1e27c83f1b50b1dc3e4588bb76e1f14b74
teinett <teine@teine.ru>
2023-04-14 18:32:51 -0600

    Update astro.config.mjs
    Added integration for sitemap and prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import node from "@astrojs/node";
+import sitemap from "@astrojs/sitemap";
+import prefetch from "@astrojs/prefetch";
 
+// https://astro.build/config
 export default defineConfig({
     site: 'https://untitled.urbansheep.com',
-    integrations: [mdx()],
+    integrations: [
+        mdx(),
+        sitemap({
+            entryLimit: 5000
+        }),
+        prefetch()
+    ],
     output: 'server',
     adapter: node({
         mode: "standalone"
