34fc1a975a1175d0809fe5bf71e3bc08062e314f
teinett <teine@teine.ru>
2023-09-02 04:16:59 -0600

    Update config.ts
    Made 10 posts instead of 7 for testing

--- src/config.ts
+++ src/config.ts
@@ -1,19 +1,19 @@
 // Consts for site and theme configuration
 export const SITE = {
-    name: 'untitled',
-    title: '*Безымянная овца',
-    description: 'Untitled Urbansheep',
-    url: 'https://untitled.urbansheep.com',
-}
+    name: "untitled",
+    title: "*Безымянная овца",
+    description: "Untitled Urbansheep",
+    url: "https://untitled.urbansheep.com",
+};
 
 // Theme configuration
 export const THEME = {
-    language: 'ru',
-    textDirection: 'ltr',
+    language: "ru",
+    textDirection: "ltr",
 
     // Posts per page (pagination) on website
-    postsPerPage: 7,
+    postsPerPage: 10,
 
     // Posts per feed on RSS
     postsPerFeed: 10,
-}
+};
