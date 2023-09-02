aa5123f9c9e81198c36d829c051f52f9584ee59d
teinett <teine@teine.ru>
2023-09-02 04:09:20 -0600

    Update astro.config.mjs
    Deleted @astrojs/mdx and @astrojs/prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from "@astrojs/mdx";
+import { defineConfig } from "astro/config";
+
 import node from "@astrojs/node";
 import sitemap from "@astrojs/sitemap";
-import prefetch from "@astrojs/prefetch";
 import netlify from "@astrojs/netlify/functions";
 
 export default defineConfig({
-    site: 'https://untitled.urbansheep.com',
+    site: "https://untitled.urbansheep.com",
     integrations: [
-        mdx(), 
         sitemap({
-            entryLimit: 5000
-        }), 
-        prefetch()
+            entryLimit: 5000,
+        }),
     ],
-    output: 'server',
-    adapter: netlify()
+    output: "server",
+    adapter: netlify(),
     // adapter: node({
     //       mode: "standalone"
     //   })
