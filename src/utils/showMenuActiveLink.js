b7e969f55b4b4da94c6ad07977b03a4003eb0eba
teinett <teine@teine.ru>
2023-09-07 19:36:54 -0600

    Updated for Images

--- src/utils/showMenuActiveLink.js
+++ src/utils/showMenuActiveLink.js
@@ -1,8 +1,21 @@
-const header = document.querySelector("header");
-const navLinks = header.querySelectorAll("[data-navLink]");
+const nav = document.querySelector("[data-menu]");
+const navLinks = nav.querySelectorAll("[data-navLink]");
+
+const currentPath = window.location.pathname;
+// if currentPath has a slash at the end, remove it
+const currentPathWithoutSlash = currentPath.endsWith("/")
+    ? currentPath.slice(0, -1)
+    : currentPath;
 
 navLinks.forEach((link) => {
-    if (link.getAttribute("href") === window.location.pathname) {
+    const linkHref = link.getAttribute("href");
+
+    // if linkHref has a slash at the end, remove it
+    const linkHrefWithoutSlash = linkHref.endsWith("/")
+        ? linkHref.slice(0, -1)
+        : linkHref;
+
+    if (linkHrefWithoutSlash === currentPathWithoutSlash) {
         link.setAttribute("aria-current", "page");
     }
-})
+});
