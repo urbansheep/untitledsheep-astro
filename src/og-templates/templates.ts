825b8050d425bc0ba341cc32d59ff85a64ff72c4
teinett <teine@teine.ru>
2023-11-21 11:41:58 -0300

    Update templates.ts

--- src/og-templates/templates.ts
+++ src/og-templates/templates.ts
@@ -7,8 +7,8 @@ export const templates: Record<string, (data?: Record<string, any>) => any> = {
             flex-direction: column;
             align-items: center;
             gap: 24px;
-        font-size: 24px;
-        padding: 100px 120px;"
+            font-size: 24px;
+            padding: 100px 120px;"
         >
             <h1 style="font-size: 48px;">Untitled Urbansheep</h1>
             <p>Безымянная овца — нерегулярное приложение к овцепедии.</p>
