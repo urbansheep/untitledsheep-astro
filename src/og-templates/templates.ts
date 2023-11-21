8cbfde0169f35427a53e8482170c095d69d252ab
teinett <teine@teine.ru>
2023-11-21 11:41:17 -0300

    Update templates.ts

--- src/og-templates/templates.ts
+++ src/og-templates/templates.ts
@@ -5,10 +5,10 @@ export const templates: Record<string, (data?: Record<string, any>) => any> = {
         html`<div
             style="display: flex;
             flex-direction: column;
-    align-items: center;
-    gap: 24px;
-    font-size: 24px;
-    padding: 100px 120px;"
+            align-items: center;
+            gap: 24px;
+        font-size: 24px;
+        padding: 100px 120px;"
         >
             <h1 style="font-size: 48px;">Untitled Urbansheep</h1>
             <p>Безымянная овца — нерегулярное приложение к овцепедии.</p>
@@ -18,10 +18,10 @@ export const templates: Record<string, (data?: Record<string, any>) => any> = {
         html`<div
             style="display: flex;
             flex-direction: column;
-    align-items: center;
-    gap: 24px;
-    font-size: 24px;
-    padding: 100px 120px;"
+            align-items: center;
+            gap: 24px;
+            font-size: 24px;
+            padding: 100px 120px;"
         >
             <h1 style="font-size: 48px;">${data?.title}</h1>
             <p>Безымянная овца — нерегулярное приложение к овцепедии.</p>
